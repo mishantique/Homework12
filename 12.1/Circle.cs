5cbf98842ff3dd814535490fa953ca69f046095c
mishantique <michvitko@mail.ru>
2024-02-11 00:00:41 +0300

    Добавьте файлы проекта.

---
+++ 12.1/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _12._1
+{
+    public static class Circle
+    {
+        public static string GetLength(double r)
+        {
+            double length = 2 * Math.PI * r;
+            return $"Длина окружности с радиусом {r} составляет {length}";
+        }
+
+        public static string GetSquare(double r)
+        {
+            double square = Math.PI * Math.Pow(r, 2);
+            return $"Площадь окружности с радиусом {r} составляет {square}";
+        }
+        public static string CheckPoint(double x0, double y0, double x, double y, double r)
+        {
+            if ((Math.Abs(x) - Math.Abs(x0) < r) && (Math.Abs(y) - Math.Abs(y0) < r))
+            {
+                return "Точка принадлежит кругу";
+            }
+            else
+            {
+                return "Точка не принадлежит кругу";
+            }
+        }
+    }
+}
---
+++ 12.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _12._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Длина окружности
+            Console.WriteLine("Введите радиус для вычисления длины окружности");
+            double r_length = Convert.ToDouble(Console.ReadLine());
+            string answer_length = Circle.GetLength(r_length);
+            Console.WriteLine(answer_length);
+            Console.WriteLine();
+
+            //Площадь круга
+            Console.WriteLine("Введите радиус для вычисления площади круга");
+            double r_square = Convert.ToDouble(Console.ReadLine());
+            string answer_square = Circle.GetSquare(r_square);
+            Console.WriteLine(answer_square);
+            Console.WriteLine();
+
+            //Принадлежность точки
+            Console.WriteLine("Введите координату x0 центра круга");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату y0 центра круга");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату x проверяемой точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату y проверяемой точки");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите радиус окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+            
+            string answer_point = Circle.CheckPoint(x0, y0, x, y, r);
+            Console.WriteLine(answer_point);
+            Console.ReadKey();
+        }
+    }
+}
